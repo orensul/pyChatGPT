242b04d073839a78ec928feb128a523849a605c7
orensultan <orens2806@gmail.com>
2023-02-08 09:23:14 +0200

    add all exps

--- src/data/propara/read_propara.py
+++ src/data/propara/read_propara.py
@@ -1,7 +1,12 @@
 
 import json
 import argparse
-
+import csv
+import os
+import time
+import openai
+from datetime import date
+from openai.error import AuthenticationError
 
 propara_train_tsv_file = 'grids.v1.train.tsv'
 propara_train_json_file = 'grids.v1.train.json'
@@ -11,12 +16,31 @@ propara_train_para_id_title = 'grids.v1.train.para_id_title.json'
 dataset_type_file = {"train": propara_train_json_file, "dev": propara_dev_json_file, "test": propara_test_json_file}
 
 original_text_files_path = '../original_text_files/'
+OPENAI_API_KEY = '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'
+openai.api_key = OPENAI_API_KEY
 
+output_csv_file = 'analogies_subjects.csv'
+output_csv_file_far = 'analogies_subjects_far.csv'
 
-def main(args):
-    """
-    Create text files with the ProPara paragraphs by reading the training json file of ProPara
-    """
+examples_txt = 'GPT3_prompts_subset.txt'
+examples_far_txt = 'GPT3_prompts_subset_far.txt'
+examples_propara_topics_txt = 'GPT3_prompts_propara_topics.txt'
+examples_csv = 'analogies_few_shot.csv'
+examples_base_target_generation = 'GPT3_prompts_analogies_fields_generate_base_target.txt'
+
+analogies_output_csv = 'GPT3_generated_analogies.csv'
+far_analogies_output_csv = 'GPT3_generated_far_analogies.csv'
+analogies_base_target_csv = 'analogies_subjects_base_target.csv'
+
+far_analogies_propara_topics_output_csv = 'GPT3_analogies_propara_topics.csv'
+
+legal_analogies_output_csv = 'GPT3_generated_legal_analogies.csv'
+
+# domains = ['Electrical Engineering', 'Mechanical Engineering', 'Chemical Engineering','Physics', 'Biology', 'Chemistry']
+
+domains = ['Engineering', 'Natural Sciences', 'Social Sciences']
+
+def create_propara_files():
     paragraph_titles = get_paragraph_titles(propara_train_tsv_file)
     d = {}
     for t in paragraph_titles:
@@ -27,15 +51,333 @@ def main(args):
     for i in range(len(data)):
         para_id_title_map[data[i]["para_id"]] = paragraph_titles[i]
 
-    write_paragraph_id_title(para_id_title_map, propara_train_para_id_title)
+    # write_paragraph_id_title(para_id_title_map, propara_train_para_id_title)
 
     converted_data = [{} for _ in range(len(data))]
     for idx, sample in enumerate(data):
-        para_id, texts = sample["para_id"], sample["texts"]
-        converted_data[idx]["para_id"], converted_data[idx]["texts"] = para_id, texts
+        para_id, para_prompt, texts = sample["para_id"], para_id_title_map[sample["para_id"]], sample["texts"]
+        converted_data[idx]["para_id"], converted_data[idx]["para_prompt"], converted_data[idx][
+            "texts"] = para_id, para_prompt, texts
 
-    write_original_text_files(converted_data)
+    return converted_data
 
+
+def create_analogy(paragraph_prompt):
+    prompt = "The subject of the Source paragraph is " + '"' + paragraph_prompt + '"' + "\n" + \
+             "Suggest another process that is analogous, but from a far academic field. What is the subject of the analogous process? please write only the subject of the analogous process as a question."
+    print(prompt)
+    target_prompt = gpt3(prompt)
+    time.sleep(1)
+    print(target_prompt)
+    prompt = "The subject of the analogous process is " + '"' + target_prompt + '"' + "\n" + \
+             "What is the field of the analogous process?" + \
+             "Write " + '"' + "The academic field of the analogous process is: " + '"' + "and then the field"
+    target_field = gpt3(prompt)
+    time.sleep(1)
+    prompt = "Explain the analogy between " + paragraph_prompt + " to the " + target_prompt + "\n"
+    explain = gpt3(prompt)
+    explain = explain.replace('.', '.\n')
+    print(explain)
+    time.sleep(1)
+    target_field = target_field.split("The academic field of the analogous process is:")[-1].strip().replace('.', '')
+    print(target_field)
+    return target_prompt, target_field, explain
+
+
+
+def exp1():
+    converted_data = create_propara_files()
+
+    header = ['source_id', 'source_subject', 'source_text', 'source_field', 'target_subject', 'target_field', 'explain']
+    data = []
+    with open(output_csv_file_far, 'r') as file:
+        csvreader = csv.reader(file)
+        processed_ids = set([row[0] for row in csvreader]) - set(['source_id'])
+
+    for paragraph in converted_data[:15]:
+        paragraph_text, paragraph_prompt, paragraph_id = "\n".join(paragraph["texts"]), paragraph["para_prompt"], paragraph[
+            "para_id"]
+        if paragraph_id in processed_ids:
+            continue
+        prompt = "Source paragraph:\n" + '"' + paragraph_text + '"' + "\n\n" + \
+                 "Please read the Source paragraph. The subject of the Source paragraph is " + '"' + paragraph_prompt + '"' + "\n" \
+                                                                                                                              "What is the academic field of the Source paragraph? " + \
+                 "write " + '"' + "The academic field of the Source paragraph is:" + '"' + " and then the field"
+        print(prompt)
+        source_field = gpt3(prompt)
+        time.sleep(1)
+        source_field = source_field.split("The academic field of the Source paragraph is:")[-1].strip().replace('.', '')
+        print(source_field)
+        target_prompt, target_field, explain = create_analogy(paragraph_prompt)
+        data.append([paragraph_id, paragraph_prompt, paragraph_text, source_field, target_prompt, target_field, explain])
+
+    if len(data) > 0:
+        with open(output_csv_file_far, 'a', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+            writer.writerows(data)
+
+
+def   process_gpt_result(result):
+    target_text, after = result.split("SUBJECT:")
+    target_text = target_text[:-1]
+    after = after.strip()
+    split = after.split("MAPPINGS:")
+    target_prompt = split[0]
+    target_mappings = ''
+    if len(split) == 2:
+        target_mappings = split[1]
+    target_prompt, target_mappings = target_prompt[:-1], target_mappings[1:]
+    return target_prompt, target_text, target_mappings
+
+
+def clean_illegal_data():
+    examples_pairs_subjects = []
+    with open(examples_csv, 'r') as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            examples_pairs_subjects.append([row[0], row[2]])
+    examples_pairs_subjects = examples_pairs_subjects[1:]
+
+    header = ['source_id', 'source_subject', 'source_text', 'target_subject', 'target_text', 'target_mappings']
+    data = []
+
+    with open(analogies_output_csv, 'r') as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            source_id, source_text, target_text, target_mappings = row[0], row[2], row[4], row[5]
+            source_subject, target_subject = row[1], row[3]
+            if source_subject == target_subject:
+                continue
+            if (source_subject, target_subject) in [(row[0], row[1]) for row in examples_pairs_subjects]:
+                continue
+            if source_id == 'source_id':
+                continue
+            data.append([source_id, source_subject, source_text, target_subject, target_text, target_mappings])
+
+    with open(legal_analogies_output_csv, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(data)
+
+
+def gpt3_generate_target_propara_topics_analogies():
+    start_paragraph_id, end_paragraph_id = 0, 20
+    converted_data = create_propara_files()
+
+    header = ['source_id', 'source_subject', 'source_domain', 'target_domain', 'target_subject', 'target_field', 'mappings']
+    data = []
+
+
+
+    with open(far_analogies_propara_topics_output_csv, 'r') as file:
+        csvreader = csv.reader(file)
+        processed_ids = set([row[0] for row in csvreader])
+
+    with open(examples_propara_topics_txt) as f:
+        examples_prompt = "".join(f.readlines())
+
+    for paragraph in converted_data[start_paragraph_id:end_paragraph_id]:
+        paragraph_prompt, paragraph_id = paragraph["para_prompt"], paragraph["para_id"]
+        if paragraph_id in processed_ids:
+            continue
+
+        source_domain_prompt = "Write the field of the following subject: " + '"' + paragraph_prompt + '"' + " Write FIELD: and then one of the following options: "
+        for target_domain in domains:
+            source_domain_prompt = source_domain_prompt + " " + target_domain + " or"
+        source_domain = gpt3(source_domain_prompt[:-2])
+        _, source_domain = source_domain.split('FIELD:')
+        source_domain = source_domain.strip()
+        time.sleep(2)
+
+        for target_domain in domains:
+            examples_and_source_paragraph_prompt = examples_prompt + "\n\n" + "Inputs:" + "\n" + "SUBJECT:" + \
+                                                   paragraph_prompt + "\n" + "TARGET_DOMAIN: One of the fields of " \
+                                                   + target_domain + "\n" + "Outputs:\n"
+            result = gpt3(examples_and_source_paragraph_prompt)
+            time.sleep(2)
+            target_subj, after = result.split('TARGET_FIELD:')
+            before, target_subj = target_subj.split('SUBJECT:')
+            target_subj = target_subj[:-1]
+            target_field, after = after.split('MAPPINGS:')
+            target_field = target_field[1:-1]
+            mappings = after[1:]
+            print(paragraph_id, paragraph_prompt, source_domain, target_domain, target_subj, target_field)
+            data.append([paragraph_id, paragraph_prompt, source_domain, target_domain, target_subj, target_field, mappings])
+
+    if len(data) > 0:
+        with open(far_analogies_propara_topics_output_csv, 'a', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+            writer.writerows(data)
+
+
+
+def gpt3_generate_far_analogous_pairs():
+    start_paragraph_id, end_paragraph_id = 0, 40
+    converted_data = create_propara_files()
+
+    header = ['source_id', 'source_subject', 'source_text', 'target_subject', 'target_domain', 'target_text', 'target_mappings']
+    data = []
+
+    with open(far_analogies_output_csv, 'r') as file:
+        csvreader = csv.reader(file)
+        processed_ids = set([row[0] for row in csvreader])
+
+    with open(examples_far_txt) as f:
+        examples_prompt = "".join(f.readlines())
+
+
+
+    for paragraph in converted_data[start_paragraph_id:end_paragraph_id]:
+        paragraph_text, paragraph_prompt, paragraph_id = "\n".join(paragraph["texts"]), paragraph["para_prompt"], \
+                                                         paragraph[
+                                                             "para_id"]
+        if paragraph_id in processed_ids:
+            continue
+
+        for target_domain in domains:
+            examples_and_source_paragraph_prompt = examples_prompt + "\n\n" + "Inputs:" + "\n" + "PARAGRAPH:\n" + \
+                                               paragraph_text + "\n" + "SUBJECT: " + paragraph_prompt + "\n" + "TARGET_DOMAIN: " + target_domain + "\n" + "Outputs:\n" + "TARGET_PARAGRAPH:\n"
+            print(examples_and_source_paragraph_prompt)
+            result = gpt3(examples_and_source_paragraph_prompt)
+            time.sleep(2)
+            print(result)
+            target_prompt, target_text, target_mappings = process_gpt_result(result)
+            data.append([paragraph_id, paragraph_prompt, paragraph_text, target_prompt, target_domain, target_text, target_mappings])
+
+    if len(data) > 0:
+        with open(far_analogies_output_csv, 'a', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+            writer.writerows(data)
+
+
+
+
+
+
+
+def gpt3_generate_analogous_pairs():
+    start_paragraph_id, end_paragraph_id = 0, 2
+    converted_data = create_propara_files()
+
+    header = ['source_id', 'source_subject', 'source_text', 'target_subject', 'target_text', 'target_mappings']
+    data = []
+
+    times_run_gpt_same_paragraph = 3
+
+    with open(analogies_output_csv, 'r') as file:
+        csvreader = csv.reader(file)
+        processed_ids = set([row[0] for row in csvreader])
+
+    with open(examples_txt) as f:
+        examples_prompt = "".join(f.readlines())
+
+    for paragraph in converted_data[start_paragraph_id:end_paragraph_id]:
+        paragraph_text, paragraph_prompt, paragraph_id = "\n".join(paragraph["texts"]), paragraph["para_prompt"], \
+                                                         paragraph[
+                                                             "para_id"]
+        examples_and_source_paragraph_prompt = examples_prompt + "\n\n" + "Inputs:" + "\n" + "PARAGRAPH:\n" + \
+                                               paragraph_text + "\n" + "SUBJECT: " + paragraph_prompt + "\n" + "Outputs:\n" + "TARGET_PARAGRAPH:\n"
+        print(examples_and_source_paragraph_prompt)
+        if paragraph_id in processed_ids:
+            continue
+
+        for _ in range(times_run_gpt_same_paragraph):
+            result = gpt3(examples_and_source_paragraph_prompt)
+            time.sleep(2)
+            print(result)
+            target_prompt, target_text, target_mappings = process_gpt_result(result)
+            data.append([paragraph_id, paragraph_prompt, paragraph_text, target_prompt, target_text, target_mappings])
+
+    if len(data) > 0:
+        with open(analogies_output_csv, 'a', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+            writer.writerows(data)
+
+def gpt3_generate_base_target_analogies():
+
+    with open(analogies_base_target_csv, 'r') as file:
+        csvreader = csv.reader(file)
+        seen_records = set()
+        for row in csvreader:
+            record = row[0] + ';' + row[1] + ';' + row[2] + ';' + row[3]
+            record = record.lower()
+            seen_records.add(record)
+
+    header = ['base_subject', 'base_field', 'target_subject', 'target_field', 'mappings']
+    data = []
+    curr_data = set()
+    for _ in range(100):
+        with open(examples_base_target_generation) as f:
+            examples_prompt = "".join(f.readlines())
+            result = gpt3(examples_prompt)
+            time.sleep(2)
+            print(result)
+            base_field, after = result.split('BASE:')
+            base_field = base_field[:-1].strip()
+
+            base_subject, after = after.split('TARGET FIELD:')
+            base_subject = base_subject[:-1].strip()
+
+            target_field, after = after.split('TARGET:')
+            target_field = target_field[:-1].strip()
+
+            target_subject, after = after.split('MAPPINGS:')
+            target_subject = target_subject[:-1].strip()
+
+            mappings = after[1:]
+
+            curr_record = base_subject + ';' + base_field + ';' + target_subject + ';' + target_field
+            curr_record = curr_record.lower()
+
+            if curr_record in seen_records:
+                continue
+
+            if curr_record in curr_data:
+                continue
+
+            data.append([base_subject, base_field, target_subject, target_field, mappings])
+            curr_data.add(curr_record)
+    if len(data) > 0:
+        with open(analogies_base_target_csv, 'a', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+            writer.writerows(data)
+
+
+
+
+
+
+def main(args):
+    # gpt3_generate_analogous_pairs()
+    # gpt3_generate_far_analogous_pairs()
+    # gpt3_generate_base_target_analogies()
+    gpt3_generate_target_propara_topics_analogies()
+
+
+
+
+
+
+def gpt3(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    if response:
+        if response.choices and response.choices[0]:
+            res = response.choices[0].text.strip()
+            return res
+    return None
 
 def write_paragraph_id_title(para_id_title_map, filename):
     with open(filename, 'w') as output_file:
--- src/pyChatGPT/__main__.py
+++ src/pyChatGPT/__main__.py
@@ -1,5 +1,8 @@
 from pyChatGPT import ChatGPT
 import os
+import openai
+
+from openai.error import AuthenticationError
 
 class Paragraph:
     def __init__(self, filename, prompt):
@@ -9,16 +12,18 @@ class Paragraph:
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-session_token = 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..wV3CPM-gYZL0nvkF.xsUE9jz48jELoIVTlRQI8P5H8dx24cCzkD0fNfH9HiuBeYsLJCscQWQ-klAfdql2f13N49nm0xgMpHvkahY6L132ocsu1kLjBVHF6M2I2bnMqd8AVqmW95Y88HHUKSLiGBYpPvozRWWmUhDiF7kYdsU9hp95qTf1x1KK8tNsRRBzna5ZC-tPNZ8msS9La84lFBn66MeVbUG44ZAO-NMNKRuK97e4tyPg0hhOWR22v6GX170-OQP9u9SJV8G1SU8aa8oo5w7Ida5T8xNUEDUMXEqMYw1XmMLjZYTgct4CLZ1hqGTr1ejg0j1bHgmwnew2a_0iURjmLm9QlJY8WP5Trt5nkLVzqNEVXeiZ2cVxH3-BKEDeQHNkUhNK7wx_jJ-RDcqLvzcNgaQS7_evy87WpmEJ1ORBj_ZC3bAy3QXNQn9H7c5N-bVywKmaJLadVNWFvWpRxw-Mp54BDrz-CRqajNu8TAtMQ4-H-aqUeA32Kwv1gYDX2pi46PyRTgZ8vfDp6Hw_qKnLDEm3xqgez3-zSjVqDN4MvGhd8d3hW0x4upojUxbiCacRnNhDxtl0QKVCf2n6L27TjfvtUqP-Or3nYXXdZVD37gvbXm5zWDphszY2HH21X9cAezMuT6jNLVquRFIt7pOMMfjO4IoZjG6Lc7XsOFcaYprCWHs93PGTpQnwY13FPiPUhHIAoiHuGqiIuP8CJZoKV7LFnbsUnIh1WFEQOWjdxT4kJ4gcIjGB4ZowRdOp6vJV8hsHUBgVaNOB9je4Q9kgsPiSJaqUeFmJLi2C5leZEuBoKLf5sD7Lc9W7LqIdiej3ikcq4OQXh6kEb1D4nUQBqYYL4PdhyzPZK4RCJf9_NRYhx-F8UupHC29BnwmRxeiSvGxoNMSxAO01w6KOkNAZaiktpLVcu8C0AtuDhRxXb5R5-cl4H0cx11q6Fg8oQbbH-m6oXi2MbzfeP63oWsevSr0oZIAlDKv-u2SueIstNzsUvaamizwY77qDhzg6XFAX4NopXVojWfGLtldzPtDTWHnppVbFdybucT5lQGPXNxEm3cgePn5lsWq_L36dDrm1CDRNWEKnhygMHJqOSou6FL-NdurOhiHCn2gMFL7BSsxjj8jTo2YV8vkpirzkdHVWLErbViyPrA8HP0WEWoegvtCjvf-YT_A23Mf8O8pO4XDKWbyci80nJ8l06Eb1OUnw0vspxGFuh7LpmxHOG8JZhRCNPjRBer8JGW0OwoPr7_RZ7QpNJZQ1NtL8_JaZd6aAI6dQsreHii2Ks6W6JrVEfyAp4Iz7jyVjRV4WxeqDOWzdyKp8lKipUMe5fhjnk9xv8YTfCyq3Y2qUaYz1ae-JtTd4IY0QbYNpFrESdGpr6avvOUCr-8-jbLxyROJjfxd4wAbZR9B_IeWsicJPqCvPsOSp9mxlQKtJgkh5DQGYd-BgNBORNJ8YFqPk0Dh0LzOG2wHDCcvEuBGhFebLvp9U42ihzXV7zdpCTZPCyPCSI124LCJ-RkxSQYcAeskjvMiqUQXHpIkeybGY_p7ip0BPzFHKdfu5VUyUyW_oth2AWpF1SwlmYB8Lw-ePBWMEO8LrUrkUDumULcnVf-SUeJzNN1jM2t8Q3be_L8WBuuEUUv_m__eTf-AFq88mdkn2NoZYIWR1aEs6LlHfBRCM3ACP2pcNemDZaaF67UOQ0IarOv5WsZ7FGsRVPqjfi_PrBFe2s3aaAlaIQuf679nJjCMExTW7SzK9YvxvRQpPhq7d-CGPoCztmrFXAX2T356IKj54S_k3d8P_eZuB7E-yrhAJiE7AQ528qesnpqnolFnDv0uorg3VT_P7qwG7EVmSLk4xXgt1OERNt50nX2SYK5LdZIR6bMVuC_rAGSrBmQKmlP8NrOhE9DQmqBQsYIFnCommonxSudDR8S4h1wTo8IAYaSSVuAj5SIz9TRFf7HwWEyvd163f_gZdSGjylrho-_50JZuFTsbS3pA9z7zgzFwcrpE0BDeGKdOBihRyFfgZI3nWHMZcJA_6koN51laeEot4ltgDABeuQ_t0mZ_PTONMM78HLqXx55OVc670QEMJlACntLdb0P0w_oFr4qhyW8Wxr7_CDjMb_GRYV9f-f5CSdON4Nx-dOK2a3OWU8aMjjj2-pvetCz_UU7N-Q7Xo8u3ye4lcq0yKl6UDaLIbSCpKdF36yfdOw0zIC9PiTwVH6mOKzLa4NuK-GI4i-hO56l-qKXF7c0QaIKtynDBj7BLTk26aZM9aCbU9F-rO0ahJpS-_dqBmANwnyjCsyYfyKpqtnSqYsWGELeKhTCxPfLY999K2DP8VzYAwocE246ZqWYqqqsiT2SOW97reTMQIBgleojaM9GE.YcRnMzYsbUgatfXcX8kGWw'
-
-text_files_folder = '../data/coref_text_files'
-
+session_token = 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..kiINyIQJTMr8lKUO.HMqEUfgVDTgvNWMLpsnJCpd6ideNnpJSwX1IDgLzd_801OctgURL5jkDx4XuXhuZnfXRX8Fuyher-L116IcYGoM7scLaBmv6ui0y1FWfGvCQ6qdMtpZjhMb5DzzoMsIUOkX-jY_4_Wy6Qwl8u_BMGdzEpv8GCL34CqSN3tJDxk5X5hagMzYu8c5zeFXjCq1TdRSWdWeI4embxh_AetQ5ieok15UGFaLZ0UX0HdBN4Xj9OIeygTvbZJESK-T_R15VvqyNFhCLn260ytREBtBtTBZ5wgaBtcVIEQ4JrUkGoXS1Gd3nLSfPK7zdD4dOZtNz_LRWxHYzXlXMAf5cWMBvrNpvHqMEfLJBCFmXhEGivoHuyxYoji0KJrwt74wt-Y7AKN5GuLhT5WiedRKd43IEbBiJ-eKEmVjy0hK-Cd04r-JJcrG_-k47-EMiQg_xVK2baHt-OSjrPVdrvi7L93RTsMOlbIzGu_Mb-iS0Zkr2mLrIaIagpctTXEFM20nAHxPofZNA1ql5E2JbvO9x78AaW-UXwxiTuuvh5R5P53nnh7VH7mM8KK1JNqh0XiDHnntDmWp6eAQyd7pImPfIel9euN5I3MJR9kCp6cYKKTnBRbIgcduLt713sW8vxIV5wABAULG_ujUsaLioCoBsI6Me4CefGOff0Hzp5Nu56VJCmp9kiEo36gwB0jDWJpySTusNWTZfuj7gmJ0W96QP938OJG1wtYQka5Mn5Bded9WspVHJl4nHvQy5GQsX0lMs4a7ON4u1xUy9yKCaSVn1wVU8Va7qLnQnhF50h2TGwgPRkN782CDxdCpcX5Zjus1cViqXJZZ_wjR3j9IDT9EvOpdsOcFyidO4wBSAEMvfWPD3DofKszFaf8R_CUucJO6kKUAfqiVgJKOCmNAYoLTGqn-v9Jq114wlQYWOffWo5hqM6fz6IG6LxBEURuRciGugjXnxXOVdkB8jVPkjdHHj_FIgOFpkJuFAL8kFlY05gTQ1Gtim5TAAPDWv_IH8cMKdPm25CZhc-BKw6W5nafdZR1qL5FVik4dUr5YUdX3JGkTV3t0sEEb-6WcWaEJIAtx_rsfEjBDu-WxOQCt-yjw5dCU3fOQPnk6Z1NhGhZ0aGqpzU8K2WFbzX3WjEmwXezYwLg5tY3b9m4JtaJujgKJix891w7ejD6XKlv5pifwlv8_WOD4n5eSNu4PsWuhVEU9F4_bCmHhIby6p73b9E9MtPJ3MevKxgUJT7uH92t70re1P8mAXy3hkGo2eGJoKRkNocPbFX8nCspZewXq-Q_QmvVkc29V1B9J0EWac43y2QS2KO55xJpkXIh4Asxr6O9dMGa3xtMv8HxKg7Oa6L7zuAplQD5lcNB3nEPvspWAoclTrpDnXS2RThmBtBqCIo5vZEBIYqvflFIvKQZgE2LS4yiaMc66Gv5ixkmmZR0qhQOz9fBA58tQ4hRPPer6UUkgel3_56n9FtO55kwtcOesWgMR7ooH2MH503pGJpBlIRJGSRm38L-BgEK9HYwl-O3jMOakMyHBd5EU9qxQve6YPFsRfbNdcCiwA7hQTkoCPyDSC9y9uL50DQBQ7dYuzgOeUaxl1DZOEfrLy11Z_EVOkYEgEKaPDl3O5GZWc4eRV7T0reQKk-LDNgrhhSZjaML7PkCvYB0_Z8qXsZYfRmzlRT5mgikXbq3J5qLm-sysydKBKvit4S4YRbw_DukGQBX4pnYUr_gXKeyARYW4efFBJloo8TKL-FabtuEwW8dQpBPinxBIWrgK8b-U5_euZ-Kyo9lQs7yTcl02VMPW4j_qu9rIZdGybt305HR58bjC1Q2ke2A-i2R-i9Sk-zGuK23Ax8h3AVnN0gest-OR7ploe44_S49MsK2HHYe9MbgLRoSONTCRTS4J6w3__iJ2aj5SamIAEhwspAozNZgoIEEK2RR_MwzdM9SzkysOGZPG_HGubNlRGMPMt0oRlfqNIJ5IUDt6zH7i8LrjIe_BdMohATNEztpsamVhywsoufGTsm5ZsFkIqZ99L7NUSy-Hm4fCAoyiIbi5IrbpN4cZGuAw-0yhAxrbALRSMidAosWcXuhcJ-AUjOmo2Su5de3sTUIFAZEcopDW-9ZlLMVczviMn-K6GSGIU-7Ykijijb5P8nhvxBsrpjJZEOKwEcQ5T55Ae1aK9LSGND781ICPUT8o637rIRuS5YH2Ir2xscjLxQPoUZUL760dpGz4UF1lRzLdbh1qZ_fNhYJCFau5AbEswSeis65jlSuHAWnhN-cT9qBUzZLaquf-FYmmHrn7jdSlY4UQ9cnywZbrGBEiWYozXv4FLyV1qL7DwFoh5eNWZUHUIfG_3x_uG00Pdhx338VA.DE5qs5HUqDSBvjEv7lejlA'
+text_files_folder = '../data/original_text_files'
 
 
+OPENAI_API_KEY = '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'
 
-example_paragraph_pairs = ( (Paragraph("animal_cell.txt", "animal cell"), Paragraph("factory.txt", "how a factory works")),
-                            (Paragraph("electrical_circuit.txt", "electrical circuit"), Paragraph("water_pump.txt", "water pump"))
-                            )
+
+
+example_paragraph_pairs = ((Paragraph("electrical_circuit.txt", "electrical circuit"), Paragraph("water_pump.txt", "water pump")),
+                           (Paragraph("animal_cell.txt", "animal_cell"), Paragraph("factory.txt", "how the factory works")))
+
+paragraphs = [Paragraph("propara_para_id_519.txt", "life cycle of the fish")]
 
 def read_file(filename):
     input_file = open(filename, 'r')
@@ -37,14 +42,25 @@ def get_inputs_from_examples():
         p1_path, p2_path = get_text_file_path(p1_filename), get_text_file_path(p2_filename)
         p1_text, p2_text = read_file(p1_path), read_file(p2_path)
 
-        input = create_input(p1_text, p2_text, p1_prompt, p2_prompt)
+        input = create_input_example(p1_text, p2_text, p1_prompt, p2_prompt)
         inputs.append(input)
     return inputs
 
+def get_inputs():
+    inputs = []
+    for paragraph in paragraphs:
+        p_prompt, p_filename = paragraph.prompt, paragraph.filename
+        p_path = get_text_file_path(p_filename)
+        p_text = read_file(p_path)
+        input = create_input(p_text, p_prompt)
+        inputs.append(input)
+        inputs.append('what is the mapping between entities in the analogous paragraphs?')
+    return inputs
 
-def run_examples(chat):
-    inputs = get_inputs_from_examples()
+def find_analogies(chat):
+    inputs = get_inputs()
     clear_screen()
+
     for input in inputs:
         print(input)
         prompt = input
@@ -58,18 +74,50 @@ def run_examples(chat):
         response = chat.send_message(prompt)
         print(response['message'], end='')
 
+    return chat
 
 
 
-def create_input(base_text, target_text, base_prompt, target_prompt):
-    input = ''
+def run_examples(chat):
+    inputs = get_inputs_from_examples()
+
+    for input in inputs:
+        print(input)
+        prompt = input
+        if prompt.lower() == 'reset':
+            chat.reset_conversation()
+            clear_screen()
+            continue
+        if prompt.lower() == 'quit':
+            break
+        print('\nChatGPT: ', end='')
+        response = chat.send_message(prompt)
+        print(response['message'], end='')
+
+    return chat
+
+
+def create_input(base_text, base_prompt):
+    input = 'Source paragraph:\n'
+    input += base_text + '\n'
+    input += 'The Source paragraph is about ' + base_prompt + '.\n'
+    input += 'Please write a Target paragraph that is analogous to the Source paragraph, no need to explain the analogy.'
+    return input
+
+def create_input_example(base_text, target_text, base_prompt, target_prompt):
     input = 'Source paragraph:\n'
     input += base_text + '\n'
     input += 'Target paragraph:\n'
     input += target_text + '\n'
     input += 'The Source paragraph is about ' + base_prompt + '.\n'
     input += 'The Target paragraph is about ' + target_prompt + '.\n'
-    input += 'Note that the two paragraphs are analogous.'
+    input += 'The two paragraphs are analogous.\n'
+    input += 'The mapping between entities in the two paragraphs:\n'
+    input += 'battery : pump\n'
+    input += 'electrical voltage : pressure\n'
+    input += 'electrons : water\n'
+    input += 'copper wire : pipe\n'
+    input += 'resistor : clams'
     return input
 
 def get_text_file_path(text_file_name):
@@ -83,9 +131,34 @@ def init_chatGPT():
         break
     return chat
 
+def gpt3():
+    openai.api_key = OPENAI_API_KEY
+
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    if response:
+        if response.choices and response.choices[0]:
+            a = response.choices[0].text
+            print(1)
+    print(1)
+
+
+
+
 if __name__ == '__main__':
-    chat = init_chatGPT()
-    run_examples(chat)
+
+    # gpt3()
+    # chat = init_chatGPT()
+    # chat = run_examples(chat)
+    # find_analogies(chat)
 
 
 
